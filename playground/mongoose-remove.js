f44bd034cb919fc32e7fd2cc4e873618f425ea52
himanshusaini111 <himanshusaini1100@gmail.com>
2018-03-29 16:20:00 -0400

    Add mongoose-remove(with out code) and DELETE /todos/:id route

---
+++ playground/mongoose-remove.js
@@ -0,0 +1,6 @@
+var {ObjectId} = require('mongodb');
+
+const {mongoose} = require('./../server/db/mongoose');
+const {Todo} = require('./../server/models/todo');
+
+
--- server/server.js
+++ server/server.js
@@ -32,14 +32,28 @@ app.get('/todos', (req, res) => {
 })
 
 app.get('/todos/:id', (req,res) => {
-    if(!ObjectId.isValid(req.params.id)) return res.status(400).send();
+    id = req.params.id;
+
+    if(!ObjectId.isValid(id)) return res.status(400).send();
     
-    Todo.findById(req.params.id).then((todo) => {
+    Todo.findById(id).then((todo) => {
         if(!todo) return res.status(404).send();
     
         res.status(200).send({todo});
     }).catch((e) => res.status(400).send());
-})
+});
+
+app.delete('/todos/:id', (req, res) => {
+    id = req.params.id;
+
+    if(!ObjectId.isValid(id)) return res.status(400).send();
+
+    Todo.findByIdAndRemove(id).then((todo) => {
+        if(!todo) return res.status(404).send();
+
+        res.status(200).send({todo});
+    }).catch((e) => res.status(400).send());
+});
 
 app.listen(port, () => {
     console.log(`listening on port ${port}`);
