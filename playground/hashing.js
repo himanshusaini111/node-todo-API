f8bfc96abe2ca30d644dec20f10d03a2dfc716d1
himanshusaini111 <himanshusaini1100@gmail.com>
2018-04-07 02:22:52 -0400

    Get passwords hashed

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,15 +1,31 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-    id:10
-};
+var pass = '123abc';
 
-var token =  jwt.sign(data, '123abc');
-console.log(token);
+bcrypt.genSalt(10, (err, salt) => {
+    bcrypt.hash(pass, salt, (err, hash) => {
+        console.log(hash);
+    })
+});
 
-var decoded = jwt.verify(token, '123abc');
-console.log(decoded);
+hashedpass = '$2a$10$JVZZysCEqbLhZAkPXD41oeUxaZh9MnkaTnaPcZxx.clNUBnySp7e2';
+              $2a$10$sRdahjRWfN80afRCV4IhYuzdDHNY6enBY41GfmUTQzxJDH6xeq3Bu
+
+bcrypt.compare(pass, hashedpass, (err, res) => {
+    console.log(res);
+})
+
+// var data = {
+//     id:10
+// };
+
+// var token =  jwt.sign(data, '123abc');
+// console.log(token);
+
+// var decoded = jwt.verify(token, '123abc');
+// console.log(decoded);
 
 // var msg = 'I am user number 3';
 // var hash = SHA256(msg).toString();
